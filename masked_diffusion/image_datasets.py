761ceaec9bb679a4b54c1f46b830961249e6ce00
whieya <whieya@gmail.com>
2023-06-23 08:06:23 +0000

    .

--- masked_diffusion/image_datasets.py
+++ masked_diffusion/image_datasets.py
@@ -6,6 +6,68 @@ import blobfile as bf
 from mpi4py import MPI
 import numpy as np
 from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+
+def load_data_mo(
+    *,
+    data_dir,
+    batch_size,
+    image_size,
+    class_cond=False,
+    deterministic=False,
+    random_crop=False,
+    random_flip=True,
+):
+    """
+    For a dataset, create a generator over (images, kwargs) pairs.
+
+    Each images is an NCHW float tensor, and the kwargs dict contains zero or
+    more keys, each of which map to a batched Tensor of their own.
+    The kwargs dict can be used for class labels, in which case the key is "y"
+    and the values are integer tensors of class labels.
+
+    :param data_dir: a dataset directory.
+    :param batch_size: the batch size of each returned pair.
+    :param image_size: the size to which images are resized.
+    :param class_cond: if True, include a "y" key in returned dicts for class
+                       label. If classes are not available and this is true, an
+                       exception will be raised.
+    :param deterministic: if True, yield results in a deterministic order.
+    :param random_crop: if True, randomly crop the images for augmentation.
+    :param random_flip: if True, randomly flip the images for augmentation.
+    """
+    if not data_dir:
+        raise ValueError("unspecified data directory")
+
+    # all_files = _list_image_files_recursively(data_dir)
+    all_files = [x for x in Path(data_dir).glob('*/*.png') if 'image' in x.name]
+
+    classes = None
+    if class_cond:
+        # Assume classes are the first part of the filename,
+        # before an underscore.
+        class_names = [0]
+        sorted_classes = {x: i for i, x in enumerate(sorted(set(class_names)))}
+        classes = [sorted_classes[x] for x in class_names]
+
+    dataset = MOVI_C(
+        all_files = all_files,
+        img_size = image_size,
+        shard=MPI.COMM_WORLD.Get_rank(),
+        num_shards=MPI.COMM_WORLD.Get_size(),
+            )
+
+    if deterministic:
+        loader = DataLoader(
+            dataset, batch_size=batch_size, shuffle=False, num_workers=1, drop_last=True
+        )
+    else:
+        loader = DataLoader(
+            dataset, batch_size=batch_size, shuffle=True, num_workers=1, drop_last=True
+        )
+    while True:
+        yield from loader
+
 
 
 def load_data(
@@ -77,6 +139,40 @@ def _list_image_files_recursively(data_dir):
         elif bf.isdir(full_path):
             results.extend(_list_image_files_recursively(full_path))
     return results
+
+# DATAset for Movi-E dataset
+class MOVI_C(Dataset):
+    def __init__(self, all_files, img_size, shard=0, num_shards=0):
+        image_paths = all_files
+        self.img_size = img_size
+        self.num_segs = num_segs
+
+        self.local_images = image_paths[shard:][::num_shards]
+        self.local_classes = classes[shard:][::num_shards]
+
+        self.transform = transforms.Compose(
+            [
+                transforms.RandomHorizontalFlip() if args.random_flip else transforms.Lambda(lambda x: x),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5], [0.5]),
+            ])
+
+        # self.transform_mask = transform_mask
+        print(f'length of the dataset : {len(self.local_images)}')
+
+    def __len__(self):
+        return len(self.local_images)
+
+    def __getitem__(self, idx):
+        arr = Image.open(self.local_images[idx]).convert('RGB')
+        if random.random() < 0.5:
+            arr = arr[:, ::-1]
+
+        arr = arr.astype(np.float32) / 127.5 - 1
+        out_dict = {}
+        if self.local_classes is not None:
+            out_dict["y"] = np.array(self.local_classes[idx], dtype=np.int64)
+        return np.transpose(arr, [2, 0, 1]), out_dict
 
 
 class ImageDataset(Dataset):
